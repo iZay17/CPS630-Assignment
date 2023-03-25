7fb7fac68859784d900ea2bbb7802d07e2f1d7ba
Isaiah Campbell <81715098+iZay17@users.noreply.github.com>
2023-03-25 00:25:42 -0400

    Update shopping-app.sql

--- shopping-app.sql
+++ shopping-app.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 18, 2023 at 02:53 PM
+-- Generation Time: Mar 25, 2023 at 05:22 AM
 -- Server version: 10.4.27-MariaDB
--- PHP Version: 8.0.25
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,23 @@ SET time_zone = "+00:00";
 --
 -- Database: `shopping-app`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `Order-Id` int(11) NOT NULL,
+  `Date_Issued` date NOT NULL,
+  `Date_Received` date NOT NULL,
+  `Total_Price` decimal(10,0) NOT NULL,
+  `Payment Code` varchar(6) NOT NULL,
+  `User-Id` varchar(6) NOT NULL,
+  `Trip-Id` varchar(6) NOT NULL,
+  `Receipt-Id` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -45,9 +62,61 @@ INSERT INTO `product` (`id`, `name`, `code`, `image`, `price`) VALUES
 (3, 'Desktop', 'desktop1', 'desktop.jpg', 300.00),
 (4, 'Tablet', 'tablet1', 'tabletresized.jpg', 800.00);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trip`
+--
+
+CREATE TABLE `trip` (
+  `Trip-Id` varchar(6) NOT NULL,
+  `Source-Code` varchar(3) NOT NULL,
+  `Destination-Code` varchar(3) NOT NULL,
+  `Distance` double NOT NULL,
+  `Truck-Id` varchar(7) NOT NULL,
+  `Price` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `truck`
+--
+
+CREATE TABLE `truck` (
+  `Truck-Id` varchar(7) NOT NULL,
+  `Truck-Code` varchar(7) NOT NULL,
+  `Availability` text NOT NULL,
+  `Code` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `User-Id` varchar(20) NOT NULL,
+  `Name` text NOT NULL,
+  `Phone` int(10) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Address` varchar(50) NOT NULL,
+  `City-Code` varchar(3) NOT NULL,
+  `Login-Id` varchar(14) NOT NULL,
+  `Password` varchar(16) NOT NULL,
+  `Balance` decimal(6,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`Order-Id`);
 
 --
 -- Indexes for table `product`
@@ -55,6 +124,25 @@ INSERT INTO `product` (`id`, `name`, `code`, `image`, `price`) VALUES
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `product_code` (`code`);
+
+--
+-- Indexes for table `trip`
+--
+ALTER TABLE `trip`
+  ADD PRIMARY KEY (`Trip-Id`),
+  ADD KEY `Truck-Id` (`Truck-Id`);
+
+--
+-- Indexes for table `truck`
+--
+ALTER TABLE `truck`
+  ADD PRIMARY KEY (`Truck-Id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`User-Id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -65,6 +153,16 @@ ALTER TABLE `product`
 --
 ALTER TABLE `product`
   MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `trip`
+--
+ALTER TABLE `trip`
+  ADD CONSTRAINT `trip_ibfk_1` FOREIGN KEY (`Truck-Id`) REFERENCES `truck` (`Truck-Id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
